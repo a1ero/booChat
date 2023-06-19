d4c49c79db011ffe84823a735d6f37b0288aa1d3
alero <50052866+a1ero@users.noreply.github.com>
2023-06-19 03:52:41 +0300

    скрытый конфиг firebase

--- components/Home.jsx
+++ components/Home.jsx
@@ -9,14 +9,14 @@ import Login from "./Login";
 import {useNavigation} from "@react-navigation/native";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyAW9LPJUQQB59t-nT6091pQ_xmxiMQypMM",
-    authDomain: "boochat-6a8d1.firebaseapp.com",
-    databaseURL: "https://boochat-6a8d1-default-rtdb.europe-west1.firebasedatabase.app/",
-    projectId: "boochat-6a8d1",
-    storageBucket: "boochat-6a8d1.appspot.com",
-    messagingSenderId: "725230703382",
-    appId: "1:725230703382:web:77d87005a392008ef17637",
-    measurementId: "G-KDG5X89HMC"
+    apiKey: "myApiKey",
+    authDomain: "myAuthDomain",
+    databaseURL: "myDatabaseURL",
+    projectId: "myProjectId",
+    storageBucket: "myStorageBucket",
+    messagingSenderId: "myMessagingSenderId",
+    appId: "myAppId",
+    measurementId: "myMeasurementId"
 };
 
 if (!firebase.apps.length) {
